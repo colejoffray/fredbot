4310f7951514c1f8a907bc369b6021267a2cc25a
Cole Joffray <colejoff@gmail.com>
2023-12-02 18:21:31 -0500

    bot set up

---
+++ index.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+
+const { Client, IntentsBitField, Guild, SlashCommandBuilder} = require('discord.js');
+
+const bot = new Client({
+    intents : [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ]
+})
+
+
+bot.on('ready', c => {
+    console.log(`🤖 ${c.user.username} is ready`)
+})
+
+bot.on('messageCreate', message => {
+    if(message.author.bot){
+        return;
+    }else {
+        if(message.content === "hello"){
+            console.log(message)
+            message.reply("hello")
+        }
+    }
+
+   
+})
+
+
+bot.login(process.env.TOKEN)
