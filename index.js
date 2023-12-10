10f1d921bef51268218fda603844ee906c2ea532
Cole Joffray <colejoff@gmail.com>
2023-12-09 21:18:35 -0500

    qr

--- index.js
+++ index.js
@@ -44,11 +44,12 @@ bot.on('interactionCreate', async interaction => {
     }
     if(interaction.commandName === 'qr'){
     let url = interaction.options.get('url').name
-    fetch(`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${url}`)
-    .then(response => console.log(response))
-    .catch(err => console.log(err))
+    // fetch(`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${url}`)
+    // .then(response => console.log(response))
+    // .catch(err => console.log(err))
+    // console.log(url);
     console.log(url);
-    interaction.reply('qr code')
+    // interaction.reply(`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${url}`)
     }
     
     
