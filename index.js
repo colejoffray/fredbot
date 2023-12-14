224fac32d6561f309c10dfe58a7eb1512a8a9a03
samuel K <samuelkarsengkhong@gmail.com>
2023-12-14 15:13:56 -0800

    added try block and fetch request to do error check if link returns a valid img

--- index.js
+++ index.js
@@ -43,12 +43,24 @@ bot.on('interactionCreate', async interaction => {
         return;
     }
     if(interaction.commandName === 'qr'){
-    let url = interaction.options.get('url').value
-    interaction.reply(`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${url}`)
-    }
-    
-    
-})
+        try {
+            const url = interaction.options.getString('url');
+            const apiResponse = await fetch(`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${url}`);
+      
+            if (!apiResponse.ok) {
+              throw new Error(`Failed to generate QR code: ${apiResponse.statusText}`);
+            }
+      
+            const buffer = await apiResponse.buffer();
+            const attachment = new MessageAttachment(buffer, 'qr-code.png');
+            interaction.reply({ files: [attachment] });
+          } catch (error) {
+            console.error('Error:', error.message);
+            interaction.reply('Failed to generate QR code.');
+          }
+        }
+      });
+      
 
 
 bot.login(process.env.TOKEN)
