eb1b66db771df60e258bca024720d73161e18128
Cole Joffray <colejoff@gmail.com>
2023-12-14 19:29:52 -0500

    changed qr code command

--- index.js
+++ index.js
@@ -43,24 +43,12 @@ bot.on('interactionCreate', async interaction => {
         return;
     }
     if(interaction.commandName === 'qr'){
-        try {
             const url = interaction.options.getString('url');
-            const apiResponse = await fetch(`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${url}`);
+            const apiResponse = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${url}`;
+            interaction.reply(apiResponse)
+    
+    }
+})
       
-            if (!apiResponse.ok) {
-              throw new Error(`Failed to generate QR code: ${apiResponse.statusText}`);
-            }
-      
-            const buffer = await apiResponse.buffer();
-            const attachment = new MessageAttachment(buffer, 'qr-code.png');
-            interaction.reply({ files: [attachment] });
-          } catch (error) {
-            console.error('Error:', error.message);
-            interaction.reply('Failed to generate QR code.');
-          }
-        }
-      });
-      
-
 
 bot.login(process.env.TOKEN)
